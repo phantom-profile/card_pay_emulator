6d6b0517e2d328ab233c63f7fe8a463565f256c3
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-30 19:27:44 +0300

    finished transaction service

--- service/transactions.py
+++ service/transactions.py
@@ -1,5 +1,5 @@
-from service.crud import Db
 from service.database import Card
+from service.models import TransactionResult, TransactionStatuses
 
 
 class TransactionPerformService:
@@ -7,20 +7,38 @@ class TransactionPerformService:
     SERVICE_COMISSION = 2 / 100
     NO_COMISSION_AMOUNT = 1000
 
-    def __init__(self, src: Card, dst: Card, amount: float, db: Db):
+    def __init__(self, src: Card, dst: Card, amount: float, db):
         self.src = src
         self.dst = dst
         self.amount = amount
         self.db = db
+        self.status = None
 
     def perform(self):
-        self.amount -= self.comission
+        self.status = self.request_transaction()
+        result = TransactionResult(
+            src_card_uuid=self.src.id,
+            dst_card_uuid=self.dst.id,
+            money_amount_usd=self.amount - self.comission,
+            status=self.status,
+            comission=self.comission
+        )
+        self.db.create_transaction(result)
+        return result
 
     @property
     def comission(self):
-        if self.amount >= self.NO_COMISSION_AMOUNT:
+        if self.status != TransactionStatuses.SUCCESS:
             return 0
-        if self.dst.bank_name == self.src.bank_name:
-            return self.amount * self.SERVICE_COMISSION
 
-        return self.amount * (self.SERVICE_COMISSION + self.CROSS_BANK_COMISSION)
+        comission_percent = 0
+        if self.dst.bank_name != self.src.bank_name:
+            comission_percent += self.CROSS_BANK_COMISSION
+        if self.amount < self.NO_COMISSION_AMOUNT:
+            comission_percent += self.SERVICE_COMISSION
+
+        return self.amount * comission_percent
+
+    def request_transaction(self):
+        # emulating API call to bank system
+        return TransactionStatuses.random()
