e52b535d9ca32ae64dcfebe189274923687e80b5
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-30 21:18:19 +0300

    added comission column

--- service/database.py
+++ service/database.py
@@ -1,10 +1,9 @@
 import datetime
 import uuid
 
-from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, Uuid, DateTime, Float, UniqueConstraint
+from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, Uuid, DateTime, Float, UniqueConstraint, or_
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-
+from sqlalchemy.orm import sessionmaker, relationship, Query
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./payments_service.db"
 
@@ -24,6 +23,16 @@ class User(Base):
     token = Column(Uuid, default=uuid.uuid4, nullable=False)
 
     cards = relationship("Card", back_populates="trusted_app")
+
+    def transactions_query(self):
+        cards_ids = Query(Card.id).where(Card.trusted_app_id == self.id).scalar_subquery()
+
+        return Query(Transaction).where(
+            or_(
+                Transaction.dst_card_id.in_(cards_ids),
+                Transaction.src_card_id.in_(cards_ids)
+            )
+        )
 
 
 class Card(Base):
@@ -53,6 +62,7 @@ class Transaction(Base):
     created_at = Column(DateTime, default=datetime.datetime.now, nullable=False)
     status = Column(String, index=True, nullable=False)
     amount_usd = Column(Float, nullable=False)
+    comission = Column(Float, nullable=False, default=0.0)
 
     src = relationship("Card", back_populates="sent_transactions", foreign_keys=[src_card_id])
     dst = relationship("Card", back_populates="received_transactions", foreign_keys=[dst_card_id])
